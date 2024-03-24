cf4d19bb5acb5140f352675e7db8121e3b133117
mynmind <mynmind@proton.me>
2024-03-24 11:59:00 +0300

    eco

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "admincourse",
+      name: "admin",
       exec_mode: "cluster",
       instances: 1,
       script: "./node_modules/nuxt/bin/nuxt.mjs",
       args: "start",
-      port: 8700,
+      port: 9000,
     },
   ],
 };
